6441838a3c3a7073d00f28b0f70266055329d49d
jaspersanchez <jaspersanchez1996@gmail.com>
2024-03-23 11:16:07 +0800

    Exercises 1.9

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,15 +1,19 @@
 import Feedback from "./Feedback"
 
 const Statistics = ({ avg, positive, good, bad, neutral }) => {
-    return (
-        <>
-            <Feedback title="good" feedback={good} />
-            <Feedback title="neutral" feedback={neutral} />
-            <Feedback title="bad" feedback={bad} />
-            <Feedback title="avg" feedback={avg} />
-            <Feedback title="positive" feedback={positive} />
-        </>
-    )
+    if (!isNaN(avg)) {
+        return (
+            <>
+                <Feedback title="good" feedback={good} />
+                <Feedback title="neutral" feedback={neutral} />
+                <Feedback title="bad" feedback={bad} />
+                <Feedback title="avg" feedback={avg} />
+                <Feedback title="positive" feedback={positive} />
+            </>
+        )
+    } 
+    
+    return <p>No feedback given</p>
 }
 
 
