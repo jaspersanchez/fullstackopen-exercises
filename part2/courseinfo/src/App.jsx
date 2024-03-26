570f672ddc6485027e56f4c79e2a0c93bf12be7d
jaspersanchez <jaspersanchez1996@gmail.com>
2024-03-26 10:13:41 +0800

    Exercises 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,4 @@
-const Header = ({ course }) => {
-  return <h2>{course}</h2>
-}
-
-const Total = ({ sum }) => {
-  return <b>total of {sum} exercises</b>
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </>
-  )
-}
-
-const Course = ({ course }) => {
-  const { name, parts } = course
-
-  const total = parts.reduce((s, p) => (s += p.exercises), 0)
-
-  return (
-    <div>
-      <Header course={name} />
-      <Content parts={parts} />
-      <Total sum={total} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
