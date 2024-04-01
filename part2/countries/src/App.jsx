f0f513f085796ddc4b73622d85a21abf4eb5f023
jaspersanchez <jaspersanchez1996@gmail.com>
2024-04-01 12:49:03 +0800

    Exercises 2.20*

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import countryService from './services/countries'
 
-const FilteredCountry = ({ country }) => {
-  console.log(country)
+const FilteredCountry = ({ country, weather }) => {
+  console.log(weather)
   return (
     <>
       <h2>{country.name.common}</h2>
@@ -15,6 +15,12 @@ const FilteredCountry = ({ country }) => {
         ))}
       </ul>
       <img src={country.flags['png']} alt={country.flags['alt']} height={150} />
+      <h2>Weather in {country.name.common} Celcius</h2>
+      <div>temperature {weather.main.temp}</div>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+      />
+      <div>wind {weather.wind.speed} m/s</div>
     </>
   )
 }
@@ -23,10 +29,20 @@ const App = () => {
   const [value, setValue] = useState('')
   const [countries, setCountries] = useState(null)
   const [selectedCountry, setSelectedCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
 
   useEffect(() => {
     countryService.getAll().then((initialData) => setCountries(initialData))
   }, [])
+
+  useEffect(() => {
+    if (selectedCountry) {
+      countryService
+        .getWeather(selectedCountry.name.common)
+        .then((initialWeather) => setWeather(initialWeather))
+        .catch((error) => console.log(error))
+    }
+  }, [selectedCountry])
 
   if (!countries) {
     return
@@ -37,6 +53,17 @@ const App = () => {
   )
 
   const handleChange = (event) => {
+    const filteredCountry = countries.filter((country) =>
+      country.name.common
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase())
+    )
+
+    if (filteredCountry.length === 1) {
+      setSelectedCountry(filteredCountry[0])
+    } else {
+      setSelectedCountry(null)
+    }
     setValue(event.target.value)
   }
 
@@ -45,24 +72,28 @@ const App = () => {
   return (
     <div>
       find countries <input value={value} onChange={handleChange} />
-      {value &&
-        (filteredCountry.length === 1 ? (
-          <FilteredCountry country={filteredCountry[0]} />
-        ) : filteredCountry.length > 10 ? (
-          <div>Too many matches, specify another filter</div>
-        ) : (
-          filteredCountry.map((country) => (
-            <div key={country.area}>
-              <span>{country.name.common}</span>{' '}
-              <button onClick={() => handleSelectedCountry(country)}>
-                show
-              </button>
-              {selectedCountry && selectedCountry.area === country.area && (
-                <FilteredCountry country={country} />
-              )}
-            </div>
-          ))
-        ))}
+      {filteredCountry.length === 1
+        ? weather && (
+            <FilteredCountry country={selectedCountry} weather={weather} />
+          )
+        : value &&
+          (filteredCountry.length > 10 ? (
+            <div>Too many matches, specify another filter</div>
+          ) : (
+            filteredCountry.map((country) => (
+              <div key={country.area}>
+                <span>{country.name.common}</span>{' '}
+                <button onClick={() => handleSelectedCountry(country)}>
+                  show
+                </button>
+                {selectedCountry &&
+                  selectedCountry.name.common === country.name.common &&
+                  weather && (
+                    <FilteredCountry country={country} weather={weather} />
+                  )}
+              </div>
+            ))
+          ))}
     </div>
   )
 }
--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,11 +1,21 @@
 import axios from 'axios'
-const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/'
+const countrybaseUrl = 'https://studies.cs.helsinki.fi/restcountries/'
+const weatherBaseUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
+const api_key = import.meta.env.VITE_API_KEY
 
 const getAll = () => {
-  const request = axios.get(`${baseUrl}/api/all`)
+  const request = axios.get(`${countrybaseUrl}/api/all`)
+  return request.then((request) => request.data)
+}
+
+const getWeather = (country) => {
+  const request = axios.get(
+    `${weatherBaseUrl}${country}&units=metric&appid=${api_key}`
+  )
   return request.then((request) => request.data)
 }
 
 export default {
   getAll,
+  getWeather,
 }
