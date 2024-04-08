0b755c693edd47f2127792c056eb6dc32d6e0c88
jaspersanchez <jaspersanchez1996@gmail.com>
2024-04-08 11:33:21 +0800

    Exercises 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -48,6 +48,13 @@ app.get('/api/persons/:id', (req, res) => {
   if (person) return res.json(person)
 
   return res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter((p) => p.id !== id)
+
+  return res.status(204).end()
 })
 
 const PORT = 3001
