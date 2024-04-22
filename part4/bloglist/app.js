a2c4239996f127fbf8ca700834a52b482dcc4890
jaspersanchez <jaspersanchez1996@gmail.com>
2024-04-22 15:01:35 +0800

    Exercises 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+
 const blogRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/users')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -21,6 +24,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogRouter)
 
 app.use(middleware.errorHandler)
