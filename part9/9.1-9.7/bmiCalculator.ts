93f0a56af9b852ba05b216b35b759a850ed52e00
jaspersanchez <48467051+jaspersanchez@users.noreply.github.com>
2025-02-03 16:41:45 +0800

    Exercises 9.1-9.3

---
+++ part9/9.1-9.7/bmiCalculator.ts
@@ -0,0 +1,43 @@
+interface bmiValues {
+  height: number;
+  weight: number;  
+}
+
+const calculateBmi = (heightCm: number, weightKg: number): string => {
+  const heightM = heightCm / 100
+  const bmi = weightKg / heightM ** 2
+
+  if (bmi >= 30) {
+    return 'Obese'
+  } else if (bmi >= 25) {
+    return 'Overweight'
+  } else if (bmi >= 18.5) {
+    return 'Normal weight'
+  } else {
+    return 'Underweight'
+  }
+}
+
+const parseBmiArguments = (args: string[]): bmiValues => {
+  if (args.length !== 4) throw new Error('Number of arguments is invalid.')
+  if (isNaN(Number(args[2])) || isNaN(Number(args[2]))) throw new Error('Arguments should be of type number.')
+  
+  return {
+    height: Number(args[2]),
+    weight: Number(args[3]),
+  }
+}
+
+try {
+ const { height, weight } = parseBmiArguments(process.argv) 
+ const bmi = calculateBmi(height, weight)
+
+ console.log(bmi)
+} catch (error: unknown) {
+ let errorMessage = 'Something went wrong: ' 
+ 
+ if (error instanceof Error) {
+   errorMessage += error.message
+ }
+ console.log(errorMessage)
+}
---
+++ part9/9.1-9.7/exerciseCalculator.ts
@@ -0,0 +1,73 @@
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+interface exerciseValues {
+  targetAmount: number,
+  dailyHours: number[]
+}
+
+const calculateExercise = ( target_amount: number, daily_hours: number[]): Result => {
+  const periodLength = daily_hours.length
+  const trainingDays = daily_hours.filter(hours => hours != 0).length
+  const target = target_amount
+  const average = daily_hours.reduce((total, num) => total + num) / periodLength
+  const success = average >= target
+  
+  let rating: number 
+  let ratingDescription: string
+
+  if (average >= target ) {
+    rating = 3    
+    ratingDescription = 'superb! training average is above target.'
+  } else if (average >= target * 0.75 ) {
+    rating = 2
+    ratingDescription = 'not too bad but could be better.'
+  } else {
+    rating = 1
+    ratingDescription = 'push harder!'
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  }
+}
+
+const parseExerciseArguments = (args: string[]): exerciseValues => {
+  if (args.length < 4) throw new Error('Number of arguments is invalid.')
+ 
+  if (isNaN(Number(args[2])) || [...args.slice(3)].some(item => isNaN(Number(item)))) {
+   throw new Error('Input should be of type number.')
+  }
+  
+  return {
+    targetAmount: Number(args[2]),
+    dailyHours: [...args.slice(3)].map(item => Number(item))
+  }
+}
+
+try {
+ const {targetAmount, dailyHours} = parseExerciseArguments(process.argv) 
+ 
+ const exerciseResult = calculateExercise(targetAmount, dailyHours)
+ console.log(exerciseResult)
+} catch (error: unknown) {
+ let errorMessage = 'Something went wrong: ' 
+ 
+ if (error instanceof Error) {
+   errorMessage += error.message
+ }
+ console.log(errorMessage)
+}
